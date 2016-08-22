d1f06fee58c8ba73d4b0a5e6419a75605c919654
SteveQ <sawicki.adam172@gmail.com>
2016-08-22 20:10:09 +0200

    Layout for second Activity

---
+++ app/src/main/java/com/steveq/scrabblehacker/PresentingResultActivity.java
@@ -0,0 +1,13 @@
+package com.steveq.scrabblehacker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresentingResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presenting_result);
+    }
+}
