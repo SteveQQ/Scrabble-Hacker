08492d702243b52fb04fa663a5d9d2232fc0caaa
SteveQ <sawicki.adam172@gmail.com>
2016-08-22 20:23:38 +0200

    make buttons work

--- app/src/main/java/com/steveq/scrabblehacker/InputLettersActivity.java
+++ app/src/main/java/com/steveq/scrabblehacker/InputLettersActivity.java
@@ -1,7 +1,9 @@
 package com.steveq.scrabblehacker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InputLettersActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class InputLettersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_letters);
+    }
+
+    public void findWords(View view){
+        Intent intent = new Intent(this, PresentingResultActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/steveq/scrabblehacker/PresentingResultActivity.java
+++ app/src/main/java/com/steveq/scrabblehacker/PresentingResultActivity.java
@@ -1,7 +1,9 @@
 package com.steveq.scrabblehacker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PresentingResultActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PresentingResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presenting_result);
+    }
+
+    public void backToInput(View view){
+        Intent intent = new Intent(this, InputLettersActivity.class);
+        startActivity(intent);
     }
 }
