538da23556248015d1220c2495c09cdc684051ed
SteveQ <sawicki.adam172@gmail.com>
2016-08-23 20:54:35 +0200

    anagrams generator added, presenting results in 2nd activity

--- app/src/main/java/com/steveq/scrabblehacker/InputLettersActivity.java
+++ app/src/main/java/com/steveq/scrabblehacker/InputLettersActivity.java
@@ -3,18 +3,45 @@ package com.steveq.scrabblehacker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class InputLettersActivity extends AppCompatActivity {
-
+    private EditText mInputArea;
+    private Button mFindButton;
+    private String input;
+    private Generator generator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_letters);
+
+        mInputArea = (EditText)findViewById(R.id.inputArea);
+        mFindButton = (Button)findViewById(R.id.findButton);
+        generator = new Generator();
+
+        mFindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input = mInputArea.getText().toString();
+                if(!input.equals("")) {
+                    Intent intent = new Intent(InputLettersActivity.this, PresentingResultActivity.class);
+                    generator.setInputWord(input);
+                    ArrayList<String> anagrams = generator.generateAnagrams(input);
+                    intent.putStringArrayListExtra("anagrams", anagrams);
+                    startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(InputLettersActivity.this, R.string.errorToast, Toast.LENGTH_LONG );
+                    toast.show();
+                }
+            }
+        });
     }
 
-    public void findWords(View view){
-        Intent intent = new Intent(this, PresentingResultActivity.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/steveq/scrabblehacker/PresentingResultActivity.java
+++ app/src/main/java/com/steveq/scrabblehacker/PresentingResultActivity.java
@@ -3,18 +3,40 @@ package com.steveq.scrabblehacker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PresentingResultActivity extends AppCompatActivity {
+
+    private TextView mResults;
+    private Button mBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presenting_result);
+
+        mResults = (TextView)findViewById(R.id.resultArea);
+        mBackButton = (Button)findViewById(R.id.backButton);
+        Intent receivedIntent = getIntent();
+        ArrayList<String> anagrams = receivedIntent.getStringArrayListExtra("anagrams");
+        StringBuilder stringBuilder = new StringBuilder();
+        for(String anagram : anagrams){
+            stringBuilder.append(anagram + "\n");
+        }
+        mResults.setText(stringBuilder);
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PresentingResultActivity.this, InputLettersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void backToInput(View view){
-        Intent intent = new Intent(this, InputLettersActivity.class);
-        startActivity(intent);
-    }
 }
